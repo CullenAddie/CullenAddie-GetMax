029572a6764529cd8cccf7185ca559e1a70e1457
CullenAddie <cullenaddie1@gmail.com>
2023-03-06 05:12:36 +0000

    Cullen A

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 public class GetMax {
     /**
      * This method should return the max value of arr (the largest int.)
@@ -7,6 +9,11 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+         int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] > max)
+            max = arr[i];
+        }
+        return max;
     }
 }
